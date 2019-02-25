6ca75443e85c2c473c261a1b9161bec93e70a862
Sam <samuel.elkind@gmail.com>
2019-02-24 20:42:08 -0700

    fraction class defined

---
+++ src/Fraction.py
@@ -0,0 +1,8 @@
+class Fraction:
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+        if denominator != 0:
+            self.denominator = denominator
+            self.value = numerator / denominator
+        else:
+            raise ZeroDivisionError

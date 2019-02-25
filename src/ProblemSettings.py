1d9cbbc8b060402e8e1fc98467072b08d163df79
Sam <samuel.elkind@gmail.com>
2019-02-24 21:59:19 -0700

    Problem Settings defined

---
+++ src/ProblemSettings.py
@@ -0,0 +1,21 @@
+class ProblemSettings:
+    def __init__(self):
+        self.term_count_min = None
+        self.term_count_max = None
+
+        self.allowed_terms = {"integers": False, "decimals": False, "fractions": False}
+        self.int_value_manual = False
+        self.int_values = []
+        self.numerator_value_manual = False
+        self.numerator_values = []
+        self.denominator_value_manual = False
+        self.denominator_values = []
+        self.decimal_value_manual = False
+        self.decimal_values = []
+
+        self.allowed_operators = {"+": False, "-": False, "*": False, "/": False}
+        self.mixed_operators = False
+        self.operator_sets = {}
+
+        self.allow_mixed_terms = False
+        self.term_sets = {}

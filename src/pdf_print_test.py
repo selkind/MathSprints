19a9c85546f79f26a0bf1739ce68bfd6744aa17b
sam <samuel.elkind@gmail.com>
2019-02-27 20:32:18 -0800

    on the right track to fix scaling in the pdf

--- src/pdf_print_test.py
+++ src/pdf_print_test.py
@@ -7,23 +7,27 @@ class Window(QtWidgets.QWidget):
         QtWidgets.QWidget.__init__(self)
         self.layout = QtWidgets.QGridLayout(self)
         self.setWindowTitle(self.tr("test rendering"))
-        self.texttest = QtWidgets.QTextEdit()
-        self.texttest.setText("FISHY FISHY FISHY")
+        self.texttest = QtWidgets.QLabel("FISHY")
         self.layout.addWidget(self.texttest)
+        self.setMinimumSize(800,800)
+
+    def print_screen(self, widget):
+        filename = "test.pdf"
+        printer = QtGui.QPdfWriter(filename)
+        printer.setPageSize(QtGui.QPdfWriter.Letter)
+        painter = QtGui.QPainter(printer)
+        painter.scale(10, 10)
+        widget.render(painter)
+        painter.end()
 
 
-def print_screen(widget):
-    filename = "test.pdf"
-    printer = QtGui.QPdfWriter(filename)
-    printer.setPageSize(QtGui.QPdfWriter.Letter)
-    painter = QtGui.QPainter(printer)
-    widget.render(painter)
-    painter.end()
-
-
-if __name__ == '__main__':
+def run():
     app = QtWidgets.QApplication([])
     window = Window()
     window.show()
-    print_screen(window)
+    window.print_screen(window.texttest)
     sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    run()

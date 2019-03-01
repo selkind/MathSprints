819b2cfd3b94f0c866e0ce6af61822bf5249af65
sam <samuel.elkind@gmail.com>
2019-03-01 14:02:49 -0800

    figured out how to set a fixed size layout

--- src/pdf_print_test.py
+++ src/pdf_print_test.py
@@ -1,15 +1,38 @@
-from PyQt5 import QtGui, QtWidgets, QtCore
+from PyQt5 import QtGui, QtWidgets, QtCore, QtQuick
 import sys
+
+
+'''Probably going to have to have two painters. One to render the worksheet dynamically
+    then another to pass the printer to when creating a pdf.'''
 
 
 class Window(QtWidgets.QWidget):
     def __init__(self):
         QtWidgets.QWidget.__init__(self)
-        self.layout = QtWidgets.QGridLayout(self)
+        self.layout = QtWidgets.QVBoxLayout(self)
         self.setWindowTitle(self.tr("test rendering"))
-        self.texttest = QtWidgets.QLabel("FISHY")
-        self.layout.addWidget(self.texttest)
-        self.setMinimumSize(800,800)
+        self.layout.addWidget(self.page_layout())
+        self.setLayout(self.layout)
+
+    def page_layout(self):
+        top_widget = QtWidgets.QWidget()
+        layout = QtWidgets.QGridLayout(top_widget)
+        label = self.config_problem_label()
+        layout.addWidget(label, 0, 1)
+        top_widget.setFixedSize(850, 1100)
+        return top_widget
+
+    def config_problem_label(self):
+        pal = QtGui.QPalette()
+        pal.setColor(QtGui.QPalette.Background, QtGui.QColor(10, 100, 10))
+        font = QtGui.QFont()
+        font.setPointSize(72)
+        font.setBold(True)
+        problem = QtWidgets.QLabel("Problem 1")
+        problem.setAutoFillBackground(True)
+        problem.setPalette(pal)
+        problem.setFont(font)
+        return problem
 
     def print_screen(self, widget):
         filename = "test.pdf"
@@ -25,7 +48,7 @@ def run():
     app = QtWidgets.QApplication([])
     window = Window()
     window.show()
-    window.print_screen(window.texttest)
+    #window.print_screen(window.texttest)
     sys.exit(app.exec_())
 
 

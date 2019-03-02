4bed1c19fa833cc37186ae04bcf25af409649ca0
sam <samuel.elkind@gmail.com>
2019-03-01 19:18:05 -0800

    margins taken care of, working on scaling

--- src/pdf_print_test.py
+++ src/pdf_print_test.py
@@ -12,26 +12,42 @@ class Window(QtWidgets.QWidget):
         QtWidgets.QWidget.__init__(self)
         self.layout = QtWidgets.QVBoxLayout(self)
         self.setWindowTitle(self.tr("test rendering"))
-        self.layout.addWidget(self.page_layout())
+        self.page = self.page_layout()
+        self.layout.addWidget(self.page)
         self.setLayout(self.layout)
 
     def page_layout(self):
         page_widget = QtWidgets.QWidget()
         pal = QtGui.QPalette()
-        pal.setColor(QtGui.QPalette.Background, QtGui.QColor(10, 100, 10))
+        pal.setColor(QtGui.QPalette.Background, QtGui.QColor(10, 100, 10, 50))
         page_widget.setAutoFillBackground(True)
         page_widget.setPalette(pal)
         layout = QtWidgets.QGridLayout(page_widget)
         label = self.problem_label(self.config_test_problem())
+        label2 = self.problem_label(self.config_test_problem())
+        layout.addWidget(label2, 1, 1)
+        print("label2 size: {}".format(label2.fontMetrics().boundingRect(label2.text())))
         layout.addWidget(label, 0, 1)
-        page_widget.setFixedSize(850, 1100)
+        page_size = self.size()
+        page_width = page_size.height() * (17/22)
+        page_widget.setFixedSize(page_width, page_size.height())
+        print(page_size)
         return page_widget
 
     def problem_label(self, problem):
-        font = QtGui.QFont()
-        font.setPointSize(12)
+        #font = QtGui.QFont()
+        #font.setPointSize(12)
+
+        pal = QtGui.QPalette()
+        pal.setColor(QtGui.QPalette.Background, QtGui.QColor(10, 10, 100, 50))
+
         problem = QtWidgets.QLabel(problem.__str__())
-        problem.setFont(font)
+        problem.setAutoFillBackground(True)
+        problem.setPalette(pal)
+        #problem.setFont(font)
+
+        #dimensions = problem.fontMetrics().boundingRect(problem.text())
+        #problem.setFixedSize(dimensions.width(), dimensions.height())
         return problem
 
     def config_test_problem(self):
@@ -42,8 +58,15 @@ class Window(QtWidgets.QWidget):
         filename = "test.pdf"
         printer = QtGui.QPdfWriter(filename)
         printer.setPageSize(QtGui.QPdfWriter.Letter)
+        printer.setPageMargins(QtCore.QMarginsF())
+
+        logicaldpix = printer.logicalDpiX()
+        pointsperinch = 40
+        printer.pageSize()
+        scaling = logicaldpix / pointsperinch
+
         painter = QtGui.QPainter(printer)
-        painter.scale(10, 10)
+        painter.scale(scaling, scaling)
         widget.render(painter)
         painter.end()
 
@@ -55,7 +78,7 @@ def run():
     #geom = window.availableGeometry()
     #print(geom)
     window.show()
-    #window.print_screen(window)
+    window.print_screen(window.page)
     sys.exit(app.exec_())
 
 

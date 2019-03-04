e6d69b1c49dfa409a385551e546143cea09be72d
sam <samuel.elkind@gmail.com>
2019-03-03 16:02:45 -0800

    basic sprint viewer test configured, ready to run

--- src/GUI/ProblemSetPageSettings.py
+++ src/GUI/ProblemSetPageSettings.py
@@ -2,6 +2,7 @@ class ProblemSetPageSettings:
     ANSWER_SPACE = 10
 
     def __init__(self):
+        self.auto_columns = True
         self.columns = 1
         self.max_problems_per_page = 50
         self.font_size = 4
---
+++ tests/SprintViewerTest.py
@@ -0,0 +1,30 @@
+from PyQt5.QtWidgets import QApplication
+from src.GUI.sprint_viewer import SprintViewer
+from src.GUI.ProblemSetPageSettings import ProblemSetPageSettings
+from tests.basic_problem_set import TestSet
+import sys
+
+
+def run():
+    app = QApplication([])
+    sprint = SprintViewer()
+    set_page_settings = ProblemSetPageSettings()
+    test_set = TestSet(50)
+
+    sprint.problem_sets.append(test_set)
+    sprint.problem_set_settings.append(set_page_settings)
+
+    sprint.layout_problem_set()
+
+    for i in sprint.pages:
+        sprint.layout.addWidget(i)
+
+    sprint.setLayout(sprint.layout)
+
+    sprint.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    run()

b69154f235e45de7af52b3ad28e40c1ecc895f09
sam <samuel.elkind@gmail.com>
2019-03-15 20:26:29 -0700

    added controls for term count and single term-type selection

--- src/GUI/user_control.py
+++ src/GUI/user_control.py
@@ -4,8 +4,11 @@ from PyQt5 import QtGui, QtWidgets
 class UserControl(QtWidgets.QFrame):
     def __init__(self):
         QtWidgets.QFrame.__init__(self)
+        self.test_term_list = ["Integer", "Fraction", "Decimal"]
         self.layout = QtWidgets.QGridLayout(self)
         self.layout.addWidget(self.page_layout_input(), 0, 0)
+        self.layout.addWidget(self.problem_set_settings(), 1, 0)
+        self.layout.addWidget(self.term_type_settings(), 2, 0)
 
     def page_layout_input(self):
         frame = QtWidgets.QFrame()
@@ -45,4 +48,62 @@ class UserControl(QtWidgets.QFrame):
         page_size_menu.addItems(page_sizes.keys())
         page_size_menu.setCurrentText("Letter")
         return page_size_menu
+
+    def problem_set_settings(self):
+        frame = QtWidgets.QFrame()
+        layout = QtWidgets.QGridLayout(frame)
+
+        layout.addWidget(self.term_count_settings(), 0, 0)
+
+        return frame
+
+    def term_count_settings(self):
+        frame = QtWidgets.QFrame()
+        layout = QtWidgets.QGridLayout(frame)
+
+        variable_term_count = QtWidgets.QCheckBox("Variable Number of Terms")
+        variable_term_count.setCheckable(True)
+        variable_term_count.setChecked(False)
+        layout.addWidget(variable_term_count, 0, 0, 1, 2)
+
+        term_count_label = QtWidgets.QLabel("Terms per Problem")
+        term_count_label_min = QtWidgets.QLabel("Minimum")
+        term_count_label_max = QtWidgets.QLabel("Maximum")
+        layout.addWidget(term_count_label, 1, 0, 1, 2)
+        layout.addWidget(term_count_label_min, 2, 0)
+        layout.addWidget(term_count_label_max, 3, 0)
+
+        term_count_min = QtWidgets.QSpinBox()
+        term_count_max = QtWidgets.QSpinBox()
+        term_count_min.setValue(2)
+        term_count_min.setMinimum(2)
+        term_count_max.setValue(term_count_min.value())
+        term_count_max.setMinimum(term_count_min.value())
+
+        layout.addWidget(term_count_min, 2, 1)
+        layout.addWidget(term_count_max, 3, 1)
+
+        return frame
+
+    def term_type_settings(self):
+        frame = QtWidgets.QFrame()
+        layout = QtWidgets.QVBoxLayout(frame)
+
+        multiple_type_option = QtWidgets.QCheckBox("Mulitple Term Types")
+        multiple_type_option.setCheckable(True)
+        multiple_type_option.setChecked(False)
+
+        layout.addWidget(multiple_type_option)
+        layout.addWidget(self.term_list_radio(self.test_term_list))
+
+        return frame
+
+    def term_list_radio(self, term_types):
+        frame = QtWidgets.QFrame()
+        layout = QtWidgets.QVBoxLayout(frame)
+        for i in term_types:
+            layout.addWidget(QtWidgets.QRadioButton(i))
+
+        return frame
+
 

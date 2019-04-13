7a1835fbdea8617d8d8dc313859a5aea321b8f3b
sam <samuel.elkind@gmail.com>
2019-04-13 16:50:56 -0700

    sloppy refactor to remove redundant calls of WorksheetDisplay.load_pages_to_viewer via UserControlManager.update_models()

--- src/GUI/controllers/problem_set_layout_manager.py
+++ src/GUI/controllers/problem_set_layout_manager.py
@@ -42,11 +42,19 @@ class ProblemSetLayoutManager:
         self.view.point_val.setValue(self.current_model.problem_value)
 
     def update_model(self):
-        self.update_v_space()
-        self.update_h_space()
-        self.update_col_num()
-        self.update_max_problems()
-        self.update_problem_val()
+        self.clear_connections()
+        self.current_model.v_answer_space = self.view.v_space.value()
+        self.current_model.h_answer_space = self.view.h_space.value()
+
+        if self.view.col_number.currentText() == "Auto":
+            self.current_model.auto_columns = True
+        else:
+            self.current_model.auto_columns = False
+            self.current_model.columns = int(self.view.col_number.currentText())
+
+        self.current_model.max_problems_per_page = self.view.page_prob_count.value()
+        self.current_model.problem_value = self.view.point_val.value()
+        self.configure_buttons()
 
     def update_v_space(self):
         self.current_model.v_answer_space = self.view.v_space.value()

30c5c7b5476eadc7988cf941b2e128e882872e51
sam <samuel.elkind@gmail.com>
2019-06-15 16:38:30 -0700

    added setup file to fix import path issues

---
+++ src/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="MathSprints",
+      version="0.1",
+      description="Math problem set generator in development",
+      url="http://github.com/selkind/mathsprints",
+      author="Sam Elkind",
+      license="GPL3.0",
+      zip_safe=False)

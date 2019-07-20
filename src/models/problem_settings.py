938272a77a46d47132ede29f4cd48f5282e5f903
sam <samuel.elkind@gmail.com>
2019-07-19 18:19:26 -0700

    removed deprecated comment

--- src/models/problem_settings.py
+++ src/models/problem_settings.py
@@ -5,38 +5,6 @@ class ProblemSettings:
         self.term_count_max = self.term_count_min
         self.ordered = True
 
-        '''
-        draft of problem element data structure.
-        
-        self.problem_elements = \
-        [
-            {
-            "terms":
-                {
-                    "Integer": [{"range": True, "vals": [0, 100]}, 
-                                 {"range": False, "vals": [-1, -2, -5]}], 
-                    "Fraction": {"Numerator": {"Integer": {"range": True, "vals": [-3, 10]}, 
-                                "Denominator": {"Integer": {"range": True, "vals": [-3, 10]}}}}
-                    "Decimal": {"precision": 3, "vals": [0, 100]}
-                },
-            "operators": ["+", "-"]
-            },
-            {
-            "terms":
-                {
-                    "Integer": {"range": True, "vals": [100, 200]}
-                },
-            "operators": ["/", "*"]
-            }
-        ]
-        
-        **Notes:
-            Numerators and denominators will initially only support Integer values, but can be expanded to be
-            recursive fractions (to a limited depth) and have Decimal values.
-            
-            Decimal values will eventually need to support ranges and discrete options for precision. 
-            Also a good idea to support integer-like user controlled selection/limitation of decimal values. 
-        '''
         # supported terms integer, fraction
 
         self.ordered_terms = [
